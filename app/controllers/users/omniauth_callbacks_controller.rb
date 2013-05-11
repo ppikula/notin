08a8e1e5de149e303af9431a670f99e791c62b2a
Piotr Świtlicki <pipboy3k@gmail.com>
2013-05-11 15:11:24 +0200

    remove debugging puts

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,9 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_filter :authenticate_user!
 
   def facebook
-    puts request.env['omniauth.auth']
     sign_in User.find_or_create_facebook_user(request.env['omniauth.auth'])
-
     redirect_to ''
   end
 end
